7c4909f062a2d0598ef8d1516a0d5ca54b737ba6
Corey T Cox <coxcorey19@gmail.com>
2020-06-09 14:03:52 -0400

    initial commit

---
+++ HelloMVC.Tests/HomeControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using HelloMVC.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace HelloMVC.Tests
+{
+    public class HomeControllerTests : HomeController
+    {
+        [Fact]
+        public void Index_Should_Return_View_Results()
+        {
+            var controller = new HomeController();
+            var result = controller.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
---
+++ HelloMVC/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMVC.Controllers
+{
+    public class HomeController
+    {
+        public ViewResult Index()
+        {
+            return null;
+        }        
+    }
+}
